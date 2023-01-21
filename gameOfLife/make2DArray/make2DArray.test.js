36f83c35d51e13c41b52cbc5bd7b55ebb8ad2c35
Victor Granda <victorgranda1992@gmail.com>
2023-01-21 19:30:20 +0100

    Add testing function for make2DArray

--- gameOfLife/make2DArray/make2DArray.test.js
+++ gameOfLife/make2DArray/make2DArray.test.js
@@ -1,6 +1,6 @@
 import make2DArray from "./make2DArray.js";
 
-describe("Given the function make2DArray ", () => {
+describe("Given the function  make2DArray ", () => {
   describe("When it receives 3 and 3 a test as arguments", () => {
     test("Then it should return a bidemnsional array of 3 test each", () => {
       const arrayColumns = 3;
